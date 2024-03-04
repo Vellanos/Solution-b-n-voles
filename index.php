8a8041cb53ba8d51780fcc41d274b44202adee73
Vellanos <davidbak38@gmail.com>
2024-03-04 15:58:18 +0100

    page accueil + liens vers autre pages

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La colonie</title>
+    <link rel="stylesheet" href="public/style/home.css">
+</head>
+
+<body>
+    <h1>La colonie</h1>
+    <div class="button-wrapper">
+        <a href="./public/pages/form.php" class="button benevole">Devenir bénévole </a>
+        <a href="./public/pages/login.php" class="button login">Se connecter</a>
+    </div>
+</body>
+
+</html>
