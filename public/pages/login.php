e17f48bee01b98de0d29d552b4f05bc50b006a87
Vellanos <davidbak38@gmail.com>
2024-03-05 13:32:05 +0100

    modif page login

--- public/pages/login.php
+++ public/pages/login.php
@@ -10,27 +10,32 @@
 
 <body>
     <?php include("../assets/components/header.php") ?>
-    <h1> Bon retour parmis nous !</h1>
+    <main class="d-flex flex-column align-items-center text-center mt-3">
+        <div class="form-wrapper d-flex flex-column gap-4">
+            <h1> Bon retour parmis nous !</h1>
+            <h2>Si vous êtes admin</h2>
+            <form class="d-flex flex-column container justify-content-center align-items-center mb-2 gap-3">
+                <div class="mb-3">
+                    <label for="Username" class="form-label d-flex">Username</label>
+                    <input type="text" class="form-control" id="Username">
+                </div>
+                <div class="mb-3">
+                    <label for="Password" class="form-label d-flex">Password</label>
+                    <input type="password" class="form-control" id="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Se connecter</button>
+            </form>
+            <h2>Si vous êtes bénévole</h2>
+            <form class="d-flex flex-column container justify-content-center align-items-center gap-3">
+                <div class="mb-3">
+                    <label for="UniqueCode" class="form-label d-flex">Unique code</label>
+                    <input type="password" class="form-control" id="UniqueCode">
+                </div>
+                <button type="submit" class="btn btn-primary">Se connecter</button>
+            </form>
+        </div>
+    </main>
 
-    <div class="form-wrapper">
-        <h2>Si vous êtes admin</h2>
-        <form action="" method="post">
-            <label for="username">Nom d'utilisateur :</label><br>
-            <input type="text" id="username" name="username" required><br>
-
-            <label for="password">Mot de passe :</label><br>
-            <input type="password" id="password" name="password" required><br><br>
-
-            <button class="form-button" type="submit">Se connecter</button>
-        </form>
-        <h2>Si vous êtes bénévole</h2>
-        <form action="" method="post">
-            <label for="unique_code">Code unique :</label><br>
-            <input type="text" id="unique_code" name="unique_code" required><br><br>
-
-            <button class="form-button" type="submit">Vérifier</button>
-        </form>
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
