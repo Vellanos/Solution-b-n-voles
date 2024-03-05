17300016a0f0b85da9ba1005934e82dbe36b3aa5
Vellanos <davidbak38@gmail.com>
2024-03-05 09:45:58 +0100

    ajout nav bar bootstrap

--- public/pages/form.php
+++ public/pages/form.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Formulaire Scindé</title>
-  <link rel="stylesheet" href="styles.css">
+  <title>La colonie</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+</head>
+
 <body>
+  <?php include("../assets/components/header.php") ?>
   <div id="partie1" class="partie">
     <!-- Partie 1 du formulaire -->
     <h2>Partie 1</h2>
@@ -39,5 +43,7 @@
   </div>
 
   <script src="../js/form.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
